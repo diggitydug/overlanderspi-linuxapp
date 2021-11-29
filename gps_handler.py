17802179bf3f1fbef2d0bfd29d784209df8d0dcf
Doug <dougsellers01@gmail.com>
2021-11-29 14:21:21 -0800

    Enable writing GPS coords to file

--- gps_handler.py
+++ gps_handler.py
@@ -1,12 +1,20 @@
 import serial
 import pynmea2
+import os
 
 #This file should access the serial NMEA data stream from
 #The USB dongle and return the current lat and lon with 
 #the get_coordinates function
 
+s_mode = os.path.exists('s.py')
+if (s_mode):
+    import s
+
 def get_coordinates():
-    return None, None
+    if(s_mode):
+       return s.s()
+    else:
+        return None, None
 
 def get_devices():
     return ['/test/path']
