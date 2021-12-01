2078e0ef1642601d7c6873bb663e2c56d00513e6
diggitydug <38229524+diggitydug@users.noreply.github.com>
2021-11-30 23:09:47 -0700

    Fix wording error in Recording files

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def start_record():
     recording_file = open(str(recording_file_path), 'a')
 
     poll_frequency = config.get_config('poll frequency')
-    file_open = "Overlander\'s Pi GPS Log\nStarted at " + file_timestr + "\nPosition Polled " + poll_frequency +' times per second \n\n'
+    file_open = "Overlander\'s Pi GPS Log\nStarted at " + file_timestr + "\nPosition Polled " + poll_frequency +' times per minute \n\n'
     
     recording_file.write(file_open)
     start_record_loop()
